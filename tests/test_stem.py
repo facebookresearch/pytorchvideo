4ceee5fb2b2286c711326d0b256ac700ee2e959e
Haoqi Fan <haoqifan@fb.com>
2020-10-20 13:42:11 -0700

    acoustic steam
    Reviewed By: tullie
    
    Differential Revision: D23212111
    
    fbshipit-source-id: f32d76a22e700d7b266eec78d7c7d4fbea8c3391

--- tests/test_stem.py
+++ tests/test_stem.py
@@ -5,7 +5,12 @@ import unittest
 
 import numpy as np
 import torch
-from pytorchvideo.models.stem import ResNetBasicStem, create_default_res_basic_stem
+from pytorchvideo.layers.convolutions import ConvReduce3D
+from pytorchvideo.models.stem import (
+    ResNetBasicStem,
+    create_default_acoustic_res_basic_stem,
+    create_default_res_basic_stem,
+)
 from torch import nn
 
 
@@ -27,6 +32,52 @@ class TestResNetBasicStem(unittest.TestCase):
                     stride=1,
                     padding=1,
                     bias=False,
+                ),
+                norm=nn.BatchNorm3d(output_dim),
+                activation=nn.ReLU(),
+                pool=None,
+            )
+
+            # Test forwarding.
+            for tensor in TestResNetBasicStem._get_inputs(input_dim):
+                if tensor.shape[1] != input_dim:
+                    with self.assertRaises(RuntimeError):
+                        output_tensor = model(tensor)
+                    continue
+                else:
+                    output_tensor = model(tensor)
+
+                input_shape = tensor.shape
+                output_shape = output_tensor.shape
+                output_shape_gt = (
+                    input_shape[0],
+                    output_dim,
+                    input_shape[2],
+                    input_shape[3],
+                    input_shape[4],
+                )
+
+                self.assertEqual(
+                    output_shape,
+                    output_shape_gt,
+                    "Output shape {} is different from expected shape {}".format(
+                        output_shape, output_shape_gt
+                    ),
+                )
+
+    def test_create_stem_with_conv_reduced_3d(self):
+        """
+        Test simple ResNetBasicStem with ConvReduce3D.
+        """
+        for input_dim, output_dim in itertools.product((2, 3), (4, 8, 16)):
+            model = ResNetBasicStem(
+                conv=ConvReduce3D(
+                    in_channels=input_dim,
+                    out_channels=output_dim,
+                    kernel_size=(3, 3),
+                    stride=(1, 1),
+                    padding=(1, 1),
+                    bias=(False, False),
                 ),
                 norm=nn.BatchNorm3d(output_dim),
                 activation=nn.ReLU(),
@@ -133,6 +184,64 @@ class TestResNetBasicStem(unittest.TestCase):
                     stride=[1, 2, 2],
                     padding=[1, 3, 3],
                     bias=False,
+                ),
+                norm=None if norm is None else norm(64),
+                activation=None if activation is None else activation(),
+                pool=None
+                if pool is None
+                else pool(kernel_size=[1, 3, 3], stride=[1, 2, 2], padding=[0, 1, 1]),
+            )
+
+            model.load_state_dict(
+                model_gt.state_dict(), strict=True
+            )  # explicitly use strict mode.
+
+            # Test forwarding.
+            for input_tensor in TestResNetBasicStem._get_inputs():
+                with torch.no_grad():
+                    if input_tensor.shape[1] != 3:
+                        with self.assertRaises(RuntimeError):
+                            output_tensor = model(input_tensor)
+                        continue
+                    else:
+                        output_tensor = model(input_tensor)
+                        output_tensor_gt = model(input_tensor)
+                self.assertEqual(
+                    output_tensor.shape,
+                    output_tensor_gt.shape,
+                    "Output shape {} is different from expected shape {}".format(
+                        output_tensor.shape, output_tensor_gt.shape
+                    ),
+                )
+                self.assertTrue(
+                    np.allclose(output_tensor.numpy(), output_tensor_gt.numpy())
+                )
+
+    def test_create_acoustic_default_stem_with_callable(self):
+        """
+        Test default builder `create_default_acoustic_res_basic_stem` with callable
+        inputs.
+        """
+        for (pool, activation, norm) in itertools.product(
+            (nn.AvgPool3d, nn.MaxPool3d, None),
+            (nn.ReLU, nn.Softmax, nn.Sigmoid, None),
+            (nn.BatchNorm3d, None),
+        ):
+            model = create_default_acoustic_res_basic_stem(
+                in_channels=3,
+                out_channels=64,
+                pool=pool,
+                activation=activation,
+                norm=norm,
+            )
+            model_gt = ResNetBasicStem(
+                conv=ConvReduce3D(
+                    in_channels=3,
+                    out_channels=64,
+                    kernel_size=((3, 1, 1), (1, 7, 7)),
+                    stride=((1, 1, 1), (1, 1, 1)),
+                    padding=((1, 0, 0), (0, 3, 3)),
+                    bias=(False, False),
                 ),
                 norm=None if norm is None else norm(64),
                 activation=None if activation is None else activation(),
