fa8c05a50ed19da55fcf54db14a1d0495254899c
Haoqi Fan <haoqifan@fb.com>
2020-10-22 19:23:12 -0700

    init
    Reviewed By: tullie, bxiong1202
    
    Differential Revision: D24441174
    
    fbshipit-source-id: e001ff7afd75cec9dfa009fae99f1322b4ad7ae7

--- pytorchvideo/models/__init__.py
+++ pytorchvideo/models/__init__.py
@@ -1,5 +1,18 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
+# Model builder with flat interface.
+from .head import create_res_basic_head
+from .stem import create_res_basic_stem
+from .csn import create_csn
+from .slowfast import create_slowfast
+from .resnet import create_resnet, create_bottleneck_block
+
+
+# Model builder with nn.Module interface.
 from .head import ResNetBasicHead  # noqa
 from .resnet import BottleneckBlock  # noqa
 from .stem import ResNetBasicStem  # noqa
+from .net import Net, MultiPathWayWithFuse
+
+# Other functions.
+from .weight_init import init_net_weights
