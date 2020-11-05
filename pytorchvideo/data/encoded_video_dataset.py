442ca6061b1c12f3a283af0d7d0927241473320b
Yuanyuan Shen <yuanyuanshen@fb.com>
2020-11-05 12:18:19 -0800

    PathManagerManifoldHandler with PyTorch Dataloader num_workers >0 Fix V2
    Summary: Fix PathManagerManifoldHandler with PyTorch Dataloader num_workers >0 by joining the multiprocess in dataset.
    
    Reviewed By: tullie
    
    Differential Revision: D24709000
    
    fbshipit-source-id: f88400bee80a4dfdf5b70ee0014291b7c5bba971

--- pytorchvideo/data/encoded_video_dataset.py
+++ pytorchvideo/data/encoded_video_dataset.py
@@ -12,7 +12,7 @@ from pytorchvideo.data.encoded_video import EncodedVideo
 
 from .labeled_video_paths import LabeledVideoPaths
 from .utils import MultiProcessSampler
-
+import multiprocessing
 
 logger = logging.getLogger(__name__)
 
@@ -192,7 +192,10 @@ def labeled_encoded_video_dataset(
                 are prefixed with this path.
 
     """
-    labeled_video_paths = LabeledVideoPaths.from_path(data_path)
+    # PathManager may configure the multiprocessing context in a way that conflicts with PyTorch DataLoader workers.
+    # To avoid this, we make sure the PathManager calls (made by LabeledVideoPaths) are wrapped in their own sandboxed process.
+    pool = multiprocessing.Pool(processes=1)
+    labeled_video_paths = pool.map(LabeledVideoPaths.from_path, [data_path])[0]
     labeled_video_paths.path_prefix = video_path_prefix
     dataset = EncodedVideoDataset(
         labeled_video_paths, clip_sampler, video_sampler, transform
