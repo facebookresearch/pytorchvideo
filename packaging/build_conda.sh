05fc437c35afc25d3e478766da2f4a204836ef5f
kalyanvasudev <kalyan051993@gmail.com>
2021-03-24 15:23:06 -0700

    testing conda build

--- packaging/build_conda.sh
+++ packaging/build_conda.sh
@@ -7,12 +7,8 @@ mkdir -p packaging/out
 version=$(python -c "exec(open('pytorchvideo/__init__.py').read()); print(__version__)")
 export BUILD_VERSION=$version
 
-export BUILD_VERSION=$version
-
-conda build -c pytorchvideo_fair \
-            -c defaults \
+conda build -c defaults \
             -c conda-forge \
-            -c anaconda \
             -c fvcore \
             -c iopath \
             --no-anaconda-upload --python \
