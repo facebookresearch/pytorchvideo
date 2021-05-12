6539ed63a1e48289636c00fcb6aa326749028819
Kalyan Vasudev Alwala <kalyanv@fb.com>
2021-05-12 12:56:50 -0700

    Add RoI Resnet Head.
    Summary: Adds a RoI head similar to basic head. However, this head can handle detection bounding boxes.
    
    Reviewed By: tullie, haooooooqi
    
    Differential Revision: D26718308
    
    fbshipit-source-id: 3d97b339a09b4e7ebb973e2aeb9c03b480a83f17

--- pytorchvideo/models/head.py
+++ pytorchvideo/models/head.py
@@ -5,6 +5,7 @@ from typing import Callable, Tuple
 import torch
 import torch.nn as nn
 from pytorchvideo.layers.utils import set_attributes
+from torchvision.ops import RoIAlign
 
 
 def create_res_basic_head(
@@ -101,6 +102,133 @@ def create_res_basic_head(
     )
 
 
+def create_res_roi_pooling_head(
+    *,
+    # Projection configs.
+    in_features: int,
+    out_features: int,
+    # RoI configs.
+    resolution: Tuple,
+    spatial_scale: float,
+    sampling_ratio: int = 0,
+    roi: Callable = RoIAlign,
+    # Pooling configs.
+    pool: Callable = nn.AvgPool3d,
+    output_size: Tuple[int] = (1, 1, 1),
+    pool_kernel_size: Tuple[int] = (1, 7, 7),
+    pool_stride: Tuple[int] = (1, 1, 1),
+    pool_padding: Tuple[int] = (0, 0, 0),
+    pool_spatial: Callable = nn.MaxPool2d,
+    # Dropout configs.
+    dropout_rate: float = 0.5,
+    # Activation configs.
+    activation: Callable = None,
+    # Output configs.
+    output_with_global_average: bool = True,
+) -> nn.Module:
+    """
+    Creates ResNet RoI head. This layer performs an optional pooling operation
+    followed by an RoI projection, an optional 2D spatial pool, an optional dropout,
+    a fully-connected projection, an activation layer
+    and a global spatiotemporal averaging.
+
+                                        Pool3d
+                                           ↓
+                                       RoI Align
+                                           ↓
+                                        Pool2d
+                                           ↓
+                                        Dropout
+                                           ↓
+                                       Projection
+                                           ↓
+                                       Activation
+                                           ↓
+                                       Averaging
+
+    Activation examples include: ReLU, Softmax, Sigmoid, and None.
+    Pool3d examples include: AvgPool3d, MaxPool3d, AdaptiveAvgPool3d, and None.
+    RoI examples include: detectron2.layers.ROIAlign, detectron2.layers.ROIAlignRotated,
+        tochvision.ops.RoIAlign and None
+    Pool2d examples include: MaxPool2e, AvgPool2d, and None.
+
+    Args:
+        Projection related configs:
+            in_features: input channel size of the resnet head.
+            out_features: output channel size of the resnet head.
+
+        RoI layer related configs:
+            resolution (tuple): h, w sizes of the RoI interpolation.
+            spatial_scale (float): scale the input boxes by this number
+            sampling_ratio (int): number of inputs samples to take for each output
+                sample interpolation. 0 to take samples densely.
+            roi (callable): a callable that constructs the roi interpolation layer,
+                examples include detectron2.layers.ROIAlign,
+                detectron2.layers.ROIAlignRotated, and None.
+
+        Pooling related configs:
+            pool (callable): a callable that constructs resnet head pooling layer,
+                examples include: nn.AvgPool3d, nn.MaxPool3d, nn.AdaptiveAvgPool3d, and
+                None (not applying pooling).
+            pool_kernel_size (tuple): pooling kernel size(s) when not using adaptive
+                pooling.
+            pool_stride (tuple): pooling stride size(s) when not using adaptive pooling.
+            pool_padding (tuple): pooling padding size(s) when not using adaptive
+                pooling.
+            output_size (tuple): spatial temporal output size when using adaptive
+                pooling.
+            pool_spatial (callable): a callable that constructs the 2d pooling layer which
+                follows the RoI layer, examples include: nn.AvgPool2d, nn.MaxPool2d, and
+                None (not applying spatial pooling).
+
+        Activation related configs:
+            activation (callable): a callable that constructs resnet head activation
+                layer, examples include: nn.ReLU, nn.Softmax, nn.Sigmoid, and None (not
+                applying activation).
+
+        Dropout related configs:
+            dropout_rate (float): dropout rate.
+
+        Output related configs:
+            output_with_global_average (bool): if True, perform global averaging on temporal
+                and spatial dimensions and reshape output to batch_size x out_features.
+    """
+    if activation is None:
+        activation_model = None
+    elif activation == nn.Softmax:
+        activation_model = activation(dim=1)
+    else:
+        activation_model = activation()
+
+    if pool is None:
+        pool_model = None
+    elif pool == nn.AdaptiveAvgPool3d:
+        pool_model = pool(output_size)
+    else:
+        pool_model = pool(
+            kernel_size=pool_kernel_size, stride=pool_stride, padding=pool_padding
+        )
+
+    if output_with_global_average:
+        output_pool = nn.AdaptiveAvgPool3d(1)
+    else:
+        output_pool = None
+
+    return ResNetRoIHead(
+        proj=nn.Linear(in_features, out_features),
+        activation=activation_model,
+        pool=pool_model,
+        pool_spatial=pool_spatial(resolution, stride=1) if pool_spatial else None,
+        roi_layer=roi(
+            output_size=resolution,
+            spatial_scale=spatial_scale,
+            sampling_ratio=sampling_ratio,
+        ),
+        dropout=nn.Dropout(dropout_rate) if dropout_rate > 0 else None,
+        output_pool=output_pool,
+    )
+
+
 class ResNetBasicHead(nn.Module):
     """
     ResNet basic head. This layer performs an optional pooling operation followed by an
@@ -146,6 +274,96 @@ class ResNetBasicHead(nn.Module):
         # Performs pooling.
         if self.pool is not None:
             x = self.pool(x)
+        # Performs dropout.
+        if self.dropout is not None:
+            x = self.dropout(x)
+        # Performs projection.
+        x = x.permute((0, 2, 3, 4, 1))
+        x = self.proj(x)
+        x = x.permute((0, 4, 1, 2, 3))
+        # Performs activation.
+        if self.activation is not None:
+            x = self.activation(x)
+
+        if self.output_pool is not None:
+            # Performs global averaging.
+            x = self.output_pool(x)
+            x = x.view(x.shape[0], -1)
+        return x
+
+
+class ResNetRoIHead(nn.Module):
+    """
+    ResNet RoI head. This layer performs an optional pooling operation
+    followed by an RoI projection, an optional 2D spatial pool, an optional dropout,
+    a fully-connected projection, an activation layer
+    and a global spatiotemporal averaging.
+                                        Pool3d
+                                           ↓
+                                       RoI Align
+                                           ↓
+                                        Pool2d
+                                           ↓
+                                        Dropout
+                                           ↓
+                                       Projection
+                                           ↓
+                                       Activation
+                                           ↓
+                                       Averaging
+
+    The builder can be found in `create_res_roi_pooling_head`.
+    """
+
+    def __init__(
+        self,
+        pool: nn.Module = None,
+        pool_spatial: nn.Module = None,
+        roi_layer: nn.Module = None,
+        dropout: nn.Module = None,
+        proj: nn.Module = None,
+        activation: nn.Module = None,
+        output_pool: nn.Module = None,
+    ) -> None:
+        """
+        Args:
+            pool (torch.nn.modules): pooling module.
+            pool_spatial (torch.nn.modules): pooling module.
+            roi_spatial (torch.nn.modules): RoI (Ex: Align, pool) module.
+            dropout(torch.nn.modules): dropout module.
+            proj (torch.nn.modules): project module.
+            activation (torch.nn.modules): activation module.
+            output_pool (torch.nn.Module): pooling module for output.
+        """
+        super().__init__()
+        set_attributes(self, locals())
+        assert self.proj is not None
+
+    def forward(self, x: torch.Tensor, bboxes: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x (torch.tensor): input tensor
+            bboxes (torch.tensor): Accociated bounding boxes.
+                The format is N*5 (Index, X_1,Y_1,X_2,Y_2) if using RoIAlign
+                and N*6 (Index, x_ctr, y_ctr, width, height, angle_degrees) if
+                using RoIAlignRotated.
+        """
+        # Performs 3d pooling.
+        if self.pool is not None:
+            x = self.pool(x)
+        # Performs roi layer using bboxes
+        if self.roi_layer is not None:
+            temporal_dim = x.shape[-3]
+            if temporal_dim != 1:
+                raise Exception(
+                    "Temporal dimension should be 1. Consider modifying the pool layer."
+                )
+            x = torch.squeeze(x, -3)
+            x = self.roi_layer(x, bboxes)
+            # Performs spatial 2d pooling.
+            if self.pool_spatial is not None:
+                x = self.pool_spatial(x)
+            x = x.unsqueeze(-3)
         # Performs dropout.
         if self.dropout is not None:
             x = self.dropout(x)
--- tests/test_models_head.py
+++ tests/test_models_head.py
@@ -5,8 +5,14 @@ import unittest
 
 import numpy as np
 import torch
-from pytorchvideo.models.head import ResNetBasicHead, create_res_basic_head
+from pytorchvideo.models.head import (
+    ResNetBasicHead,
+    ResNetRoIHead,
+    create_res_basic_head,
+    create_res_roi_pooling_head,
+)
 from torch import nn
+from torchvision.ops import RoIAlign
 
 
 class TestHeadHelper(unittest.TestCase):
@@ -169,3 +175,203 @@ class TestHeadHelper(unittest.TestCase):
         )
         for shape in shapes:
             yield torch.rand(shape)
+
+
+class TestRoIHeadHelper(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        torch.set_rng_state(torch.manual_seed(42).get_state())
+
+    def test_build_simple_head(self):
+        """
+        Test simple ResNetRoIHead
+        (without pool_spatial, roi, dropout and activation layers).
+        """
+        for input_dim, output_dim in itertools.product((4, 8), (4, 8, 16)):
+            model = ResNetRoIHead(
+                proj=nn.Linear(input_dim, output_dim),
+                pool=nn.AdaptiveAvgPool3d(1),
+                output_pool=nn.AdaptiveAvgPool3d(1),
+            )
+            bboxes = None
+
+            # Test forwarding.
+            for input_tensor in TestHeadHelper._get_inputs(input_dim=input_dim):
+                if input_tensor.shape[1] != input_dim:
+                    with self.assertRaises(RuntimeError):
+                        output_tensor = model(input_tensor, bboxes)
+                    continue
+                else:
+                    output_tensor = model(input_tensor, bboxes)
+
+                input_shape = input_tensor.shape
+                output_shape = output_tensor.shape
+                output_shape_gt = (input_shape[0], output_dim)
+
+                self.assertEqual(
+                    output_shape,
+                    output_shape_gt,
+                    "Output shape {} is different from expected shape {}".format(
+                        output_shape, output_shape_gt
+                    ),
+                )
+
+    def test_build_complex_head(self):
+        """
+        Test complex ResNetRoIHead.
+        """
+        # ROI layer configs
+        resolution = (10, 15)
+        spatial_scale = 1.0 / 5.0
+        sampling_ratio = 0
+        roi_layer = RoIAlign(
+            resolution, spatial_scale=spatial_scale, sampling_ratio=sampling_ratio
+        )
+
+        for input_dim, output_dim in itertools.product((4, 8), (4, 8, 16)):
+
+            model = ResNetRoIHead(
+                proj=nn.Linear(input_dim, output_dim),
+                activation=nn.Softmax(),
+                pool=nn.AdaptiveAvgPool3d(1),
+                pool_spatial=nn.MaxPool2d(resolution, stride=1),
+                roi_layer=roi_layer,
+                dropout=nn.Dropout(0.5),
+                output_pool=nn.AdaptiveAvgPool3d(1),
+            )
+
+            # Test forwarding.
+            for (input_tensor, bboxes) in TestRoIHeadHelper._get_inputs(
+                input_dim=input_dim
+            ):
+                if input_tensor.shape[1] != input_dim:
+                    with self.assertRaises(Exception):
+                        output_tensor = model(input_tensor, bboxes)
+                    continue
+                output_tensor = model(input_tensor, bboxes)
+
+                bboxes_shape = bboxes.shape
+                output_shape = output_tensor.shape
+                output_shape_gt = (bboxes_shape[0], output_dim)
+                self.assertEqual(
+                    output_shape,
+                    output_shape_gt,
+                    "Output shape {} is different from expected shape {}".format(
+                        output_shape, output_shape_gt
+                    ),
+                )
+
+    def test_build_head_with_callable(self):
+        """
+        Test builder `create_res_roi_pooling_head`.
+        """
+        # ROI layer configs
+        resolution = (10, 15)
+        spatial_scale = 1.0 / 5.0
+        sampling_ratio = 0
+        roi_layer = RoIAlign(
+            resolution, spatial_scale=spatial_scale, sampling_ratio=sampling_ratio
+        )
+
+        for (pool, activation) in itertools.product(
+            (nn.AvgPool3d, nn.MaxPool3d, nn.AdaptiveAvgPool3d, None),
+            (nn.ReLU, nn.Softmax, nn.Sigmoid, None),
+        ):
+            if activation is None:
+                activation_model = None
+            elif activation == nn.Softmax:
+                activation_model = activation(dim=1)
+            else:
+                activation_model = activation()
+
+            if pool is None:
+                pool_model = None
+            elif pool == nn.AdaptiveAvgPool3d:
+                pool_model = pool(1)
+            else:
+                pool_model = pool(kernel_size=[5, 1, 1], stride=[1, 1, 1])
+
+            model = create_res_roi_pooling_head(
+                in_features=16,
+                out_features=32,
+                resolution=resolution,
+                spatial_scale=spatial_scale,
+                sampling_ratio=sampling_ratio,
+                roi=RoIAlign,
+                pool=pool,
+                pool_spatial=nn.MaxPool2d,
+                pool_kernel_size=(5, 1, 1),
+                output_size=(1, 1, 1),
+                dropout_rate=0.0,
+                activation=activation,
+                output_with_global_average=True,
+            )
+            model_gt = ResNetRoIHead(
+                proj=nn.Linear(16, 32),
+                activation=activation_model,
+                pool=pool_model,
+                pool_spatial=nn.MaxPool2d(resolution, stride=1),
+                roi_layer=roi_layer,
+                dropout=None,
+                output_pool=nn.AdaptiveAvgPool3d(1),
+            )
+            model.load_state_dict(
+                model_gt.state_dict(), strict=True
+            )  # explicitly use strict mode.
+
+            # Test forwarding.
+            for (input_tensor, bboxes) in TestRoIHeadHelper._get_inputs(input_dim=16):
+                with torch.no_grad():
+                    if (
+                        input_tensor.shape[1] != 16
+                        or (pool is None)
+                        or (
+                            input_tensor.shape[-3] != 5 and pool != nn.AdaptiveAvgPool3d
+                        )
+                    ):
+                        with self.assertRaises(Exception):
+                            output_tensor = model(input_tensor, bboxes)
+                        continue
+                    else:
+                        output_tensor = model(input_tensor, bboxes)
+                        output_tensor_gt = model_gt(input_tensor, bboxes)
+                self.assertEqual(
+                    output_tensor.shape,
+                    output_tensor_gt.shape,
+                    "Output shape {} is different from expected shape {}".format(
+                        output_tensor.shape, output_tensor_gt.shape
+                    ),
+                )
+                self.assertTrue(
+                    np.allclose(output_tensor.numpy(), output_tensor_gt.numpy())
+                )
+
+    @staticmethod
+    def _get_inputs(input_dim: int = 8) -> torch.tensor:
+        """
+        Provide different tensors as test cases.
+
+        Yield:
+            (torch.tensor): tensor as test case input.
+            (torch.tensor): tensor as test case bboxes.
+        """
+        # Prepare random tensor as test cases.
+        shapes = (
+            # Forward succeeded.
+            (1, input_dim, 5, 7, 7),
+            (2, input_dim, 5, 7, 7),
+            (4, input_dim, 5, 7, 7),
+            (4, input_dim, 5, 7, 7),
+            (4, input_dim, 7, 7, 7),
+            (4, input_dim, 7, 7, 14),
+            (4, input_dim, 7, 14, 7),
+            (4, input_dim, 7, 14, 14),
+            # Forward failed.
+            (8, input_dim * 2, 3, 7, 7),
+            (8, input_dim * 4, 5, 7, 7),
+        )
+        for shape in shapes:
+            input_tensor = torch.rand(shape)
+            bboxes = [[i, 1, 2, 3, 4] for i in range(input_tensor.shape[0])]
+            bboxes = torch.Tensor(bboxes)
+            yield (input_tensor, bboxes)
